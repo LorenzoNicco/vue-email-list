ce5e2f862270efe264a045c36cc9f6606cb63bce
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-01-31 15:01:22 +0100

    Add generazione 10 email random e stampa in pagina

--- js/script.js
+++ js/script.js
@@ -5,7 +5,15 @@ const { createApp } = Vue;
 createApp ({
     data () {
         return {
-            
+            randomEmail: []
+        }
+    },
+    mounted () {
+        for(let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then ((resp) => {
+                this.randomEmail.push(resp.data.response);
+            });
         }
     }
 }).mount('#app');
